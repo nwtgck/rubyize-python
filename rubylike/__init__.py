134e6e3f68f5465b3e400b170eebc12bda36a18a
Ryo Ota <nwtgck@gmail.com>
2018-01-06 20:48:05 +0900

    [add/test] Add some methods to list, range, map, filter class

--- rubylike/__init__.py
+++ rubylike/__init__.py
@@ -1,3 +1,46 @@
-# TODO Remove
-def hello():
-  return "hello, world"
+from forbiddenfruit import curse
+
+# === common extensions ===
+
+def common_to_list(self):
+    return list(self)
+
+def common_map(self, f):
+    return map(f, self)
+
+def common_filter(self, p):
+    return filter(p, self)
+
+# === object class extensions ===
+
+def object_to_str(self):
+    return str(self)
+
+curse(object, 'to_str', object_to_str)
+
+# === list class extesions ===
+def list_length(self):
+    return len(self)
+
+curse(list, 'map',    common_map)
+curse(list, 'filter', common_filter)
+curse(list, 'length', list_length)
+
+# === range class extesions ===
+
+curse(range, "to_list", common_to_list)
+curse(range, "map",     common_map)
+curse(range, "filter",  common_filter)
+
+
+# === map class extesions ===
+
+curse(map, "to_list", common_to_list)
+curse(map, "map",     common_map)
+curse(map, "filter",  common_filter)
+
+# === filter class extesions ===
+
+curse(filter, "to_list", common_to_list)
+curse(filter, "map",     common_map)
+curse(filter, "filter",  common_filter)
--- tests/rubylike_test.py
+++ tests/rubylike_test.py
@@ -2,8 +2,31 @@ import unittest
 import rubylike
 
 class RubylikeTest(unittest.TestCase):
-  assert(rubylike.hello() == "hello, world")
-  # TODO impl
+
+  def test_list_length(self):
+    """
+    Test of list#length
+    """
+    actual = [1, 2, 3].length()
+    expect = 3
+    self.assertEqual(actual, expect)
+
+
+  def test_list_map(self):
+    """
+    Test of list#map
+    """
+    actual = [1, 2, 3].map(lambda x: x*2).to_list()
+    expect = [2, 4, 6]
+    self.assertEqual(actual, expect)
+
+  def test_list_filter(self):
+    """
+    Test of list#filter
+    """
+    actual = [1, 2, 3].filter(lambda x: x % 2 == 0).to_list()
+    expect = [2]
+    self.assertEqual(actual, expect)
 
 
 def suite():
